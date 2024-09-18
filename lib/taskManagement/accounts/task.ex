531e6e178a0afc419c37020b6bd5d2bee8d271f7
Shweta <131133867+shweta1129@users.noreply.github.com>
2024-09-18 21:16:29 +0530

    Defined belongsTo relationship with User

--- lib/taskManagement/accounts/task.ex
+++ lib/taskManagement/accounts/task.ex
@@ -2,12 +2,14 @@ defmodule TaskManagement.Accounts.Task do
   use Ecto.Schema
   import Ecto.Changeset
 
+    @derive {Jason.Encoder, only: [:id, :title, :description, :due_date, :status, :user_id, :inserted_at, :updated_at]}
+
   schema "tasks" do
     field :status, :string
     field :description, :string
     field :title, :string
     field :due_date, :date
-    field :user_id, :id
+    belongs_to :user, TaskManagement.Accounts.User
 
     timestamps(type: :utc_datetime)
   end
@@ -15,7 +17,8 @@ defmodule TaskManagement.Accounts.Task do
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:title, :description, :due_date, :status])
-    |> validate_required([:title, :description, :due_date, :status])
+    |> cast(attrs, [:title, :description, :due_date, :status, :user_id])
+    |> validate_required([:title, :status, :user_id])
+    |> validate_inclusion(:status, ["To Do", "In Progress", "Done"])
   end
 end
