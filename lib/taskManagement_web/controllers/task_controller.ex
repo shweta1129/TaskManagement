b09af9eb17d9898cb0735c079d98b746bcbca9df
Shweta <131133867+shweta1129@users.noreply.github.com>
2024-09-18 21:26:21 +0530

    Add create action to TaskController for creating user tasks

---
+++ lib/taskManagement_web/controllers/task_controller.ex
@@ -0,0 +1,25 @@
+defmodule TaskManagementWeb.TaskController do
+  use TaskManagementWeb, :controller
+  alias TaskManagement.Accounts
+
+  action_fallback TaskManagementWeb.FallbackController
+
+  # api to create task for a user
+  def create(conn, %{"user_id" => user_id, "task" => task_params}) do
+    task_params = Map.put(task_params, "user_id", user_id)
+
+    case Accounts.create_task(task_params) do
+      {:ok, task} ->
+        conn
+        |> put_status(:created)
+        |> json(%{message: "Task created successfully", task: task})
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> put_view(TaskManagementWeb.ChangesetView)
+        |> render("error.json", changeset: changeset)
+    end
+  end
+
+end
