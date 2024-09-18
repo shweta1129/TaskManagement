97a8437cc6a1b9c3deb6bc21f690426a57f4de60
Shweta <131133867+shweta1129@users.noreply.github.com>
2024-09-18 17:49:08 +0530

    Add module to handle errors

---
+++ lib/taskManagement_web/controllers/fallback_controller.ex
@@ -0,0 +1,17 @@
+defmodule TaskManagementWeb.FallbackController do
+  use TaskManagementWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(TaskManagementWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(TaskManagementWeb.ErrorView)
+    |> render(:"404")
+  end
+end
