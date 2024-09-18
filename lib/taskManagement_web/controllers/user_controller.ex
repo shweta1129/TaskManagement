e26f94e83242f70a1fd6020a8b352e203b94515d
Shweta <131133867+shweta1129@users.noreply.github.com>
2024-09-18 18:03:32 +0530

    Add create user functionality

---
+++ lib/taskManagement_web/controllers/user_controller.ex
@@ -0,0 +1,26 @@
+defmodule TaskManagementWeb.UserController do
+  use TaskManagementWeb, :controller
+  alias TaskManagement.Accounts
+
+  action_fallback TaskManagementWeb.FallbackController
+
+  # create users
+  def create(conn, %{"user" => user_params}) do
+    IO.inspect(user_params, label: "Received user_params")
+
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        IO.inspect(user, label: "Created user")
+        conn
+        |> put_status(:created)
+        |> json(%{message: "User created successfully", user: user})
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> put_view(TaskManagementWeb.ChangesetView)
+        |> render("error.json", changeset: changeset) 
+      end
+      end
+
+end
